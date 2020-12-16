99bd7d6c7aaacabfe332abcf1cd15269f49d0554
Jaafar Ahmad <jaafar95ahmad@gmail.com>
2020-12-16 03:58:23 -0600

    16/12/2020.1

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -10,6 +10,12 @@ export class AppComponent implements OnInit {
   title = 'The Dating App';
   users:any;
   
+  //Here is too early to make an http request this is just for construct the component
+  //and make sure we have got the GDP service available
+  //we could do it in the constructor its still work but its too early.
+  //The angular come with lifecycle events and after the constructor the event that
+  //will take place is knwon as the initiliazation and to use that life cycle event we
+  //implement the OnInit interface.
   constructor(private http: HttpClient) {}
   
   ngOnInit() {
@@ -17,7 +23,7 @@ export class AppComponent implements OnInit {
   }
 
   getUsers(){
-    this.http.get("https://localhost:5001/api/user").subscribe(response => {
+    this.http.get("https://localhost:5001/api/users").subscribe(response => {
     this.users = response;
     },error => {
     console.log(error);
