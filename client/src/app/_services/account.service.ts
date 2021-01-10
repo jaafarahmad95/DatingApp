7e2274d030009eaaa5f5ef5593b88da2f48e47f0
Jaafar Ahmad <jaafar95ahmad@gmail.com>
2021-01-10 15:21:49 -0600

    End of session4

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = 'https://localhost:5001/api/';
+  constructor(private http: HttpClient) {}
+  //ReplaySubject is a type of Observable that work like a buffer store inside
+  //it values and in any time a subscriber subscribe he can emitt value or multiple
+  //valuses from the buffer and here we want just one version of the current
+  //user so we detremain the size to 1
+  private currentUserSource = new ReplaySubject<User>(1);
+  //and here we convert from the replaySubject<User> to Observable<User>
+  currentUser$ = this.currentUserSource.asObservable();
+
+
+  login(model: any){
+    return this.http.post(this.baseUrl+'account/login',model).pipe(
+      map( (response: User) => {
+          const user = response;
+          if (user) {
+            localStorage.setItem('user', JSON.stringify(user));
+            this.currentUserSource.next(user);
+          }
+        })//map
+    )//pipe
+  }//fun
+
+  setCurrentUser(user : User){
+    this.currentUserSource.next(user);
+  }
+
+  LogOut () {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
+  }
+
+}
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -9,24 +11,21 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   title = 'The Dating App';
   users:any;
-  
+
   //Here is too early to make an http request this is just for construct the component
   //and make sure we have got the Http service available
   //we could do it in the constructor its still work but its too early.
   //The angular come with lifecycle events and after the constructor the event that
   //will take place is knwon as the initiliazation and to use that life cycle event we
   //implement the OnInit interface.
-  constructor(private http: HttpClient) {}
-  
+  constructor( private accountService: AccountService) {}
+
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
 
-  getUsers(){
-    this.http.get("https://localhost:5001/api/users").subscribe(response => {
-    this.users = response;
-    },error => {
-    console.log(error);
-    }); 
+  setCurrentUser() {
+      const user: User = JSON.parse(localStorage.getItem('user'));
+      this.accountService.setCurrentUser(user);
   }
 }
