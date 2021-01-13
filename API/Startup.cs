8abefc4e7037081b8f2c3fa127e8fd29a977579d
Jaafar Ahmad <jaafar95ahmad@gmail.com>
2021-01-13 06:50:10 -0600

    13-1-2021

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Extensions;
 using API.Interfaces;
+using API.Middleware;
 using API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -49,11 +50,8 @@ namespace API
         //Inside here the order is very important 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            app.UseMiddleware<ExceptionMiddleware>();
+          
             app.UseHttpsRedirection();
 
             app.UseRouting();
